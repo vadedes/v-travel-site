cbfd8837b93875601f4e3aabba8bc98911df9efb
dave <dave@vadedesign.com>
2021-12-11 18:27:35 +0800

    Completed Header Element

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,32 +1,8 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
 
 if (module.hot) {
     module.hot.accept();
 }
 
-// lesson example code
-
-//objects can contain data and methods within them
-// let john = {
-//     name: 'John Doe',
-//     favoriteColor: 'blue',
-//     greet: function () {
-//         console.log(`Hello, my name is ${this.name} and my favorite color is ${this.favoriteColor}`);
-//     },
-// };
-
-// john.greet();
-
-function Person(fullName, favColor) {
-    this.name = fullName;
-    this.favoriteColor = favColor;
-    this.greet = function () {
-        console.log(`Hello my name is ${this.name} and my favorite color is ${this.favoriteColor}.`);
-    };
-}
-
-let john = new Person('John Doe', 'blue');
-john.greet();
-
-let jane = new Person('Jane Smith', 'green');
-jane.greet();
+let mobileMenu = new MobileMenu();

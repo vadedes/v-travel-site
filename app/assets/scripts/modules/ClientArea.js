c763d0ffc5134d06bf068f2c1810ffd727dd51ce
dave <dave@vadedesign.com>
2021-12-15 02:58:54 +0800

    Corrected typo pm html classes spelling

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -3,9 +3,9 @@ import axios, { Axios } from 'axios';
 class ClientArea {
     constructor() {
         this.injectHTML();
-        this.form = document.querySelector('.client-area_form');
-        this.field = document.querySelector('.client-area_input');
-        this.contentArea = document.querySelector('.client-area_content-area');
+        this.form = document.querySelector('.client-area__form');
+        this.field = document.querySelector('.client-area__input');
+        this.contentArea = document.querySelector('.client-area__content-area');
         this.events();
     }
 

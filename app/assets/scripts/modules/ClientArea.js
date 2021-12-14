79a3ca23e836af4ea88c424116a8c2830342791c
dave <dave@vadedesign.com>
2021-12-15 03:09:43 +0800

    correct error class mistake

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -24,7 +24,7 @@ class ClientArea {
                 this.contentArea.innerHTML = response.data;
             })
             .catch(() => {
-                this.contentArea.innerHTML = `<p class="client-are__error">That secret phrase is not correct. Try again.</p>`;
+                this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again.</p>`;
                 this.field.value = '';
                 this.field.focus();
             });

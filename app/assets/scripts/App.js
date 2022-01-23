31123075e8d0522c7f55929a2733c5bc9f740a2a
dave <dave@vadedesign.com>
2022-01-23 21:16:46 +0800

    Updated vulnerability issues

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,6 +5,16 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import ClientArea from './modules/ClientArea';
 
+//React Related Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Import react components
+import MyAmazingComponent from './modules/MyAmazingComponent';
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector('#my-react-example'));
+
+//Main JS Code
 new ClientArea();
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyAmazingComponent() {
+    return (
+        <div className='wrapper'>
+            <h1 className='section-title section-title--blue'>This is My Amazing React Component</h1>
+            <p className='headline headline--centered headline--light headline--b-margin-small'>React is great!!!</p>
+        </div>
+    );
+}
+
+export default MyAmazingComponent;

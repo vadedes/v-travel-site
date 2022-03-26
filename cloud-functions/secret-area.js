7c7e10a3c8af2c898086392eb4910428f901ff92
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-26 19:29:58 +0800

    Updated dependencies

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,25 +1,25 @@
 exports.handler = function (event, context, callback) {
-    const secretContent = `
+  const secretContent = `
         <h3>Welcome To The Secret Area</h3>
         <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
     `;
 
-    let body;
+  let body;
 
-    if (event.body) {
-        body = JSON.parse(event.body);
-    } else {
-        body = {};
-    }
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
 
-    if (body.password == 'javascript') {
-        callback(null, {
-            statusCode: 200,
-            body: secretContent,
-        });
-    } else {
-        callback(null, {
-            statusCode: 401,
-        });
-    }
+  if (body.password === 'javascript') {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -7,91 +7,98 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const fse = require('fs-extra');
 
 const postCSSPlugins = [
-    require('postcss-import'),
-    require('postcss-mixins'),
-    require('postcss-simple-vars')({ silent: true }),
-    require('postcss-nested'),
-    require('autoprefixer'),
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars')({ silent: true }),
+  require('postcss-nested'),
+  require('autoprefixer'),
 ];
 
 class RunAfterCompile {
-    apply(compiler) {
-        compiler.hooks.done.tap('Copy images', function () {
-            fse.copySync('./app/assets/images', './docs/assets/images');
-        });
-    }
+  apply(compiler) {
+    compiler.hooks.done.tap('Copy images', function () {
+      fse.copySync('./app/assets/images', './docs/assets/images');
+    });
+  }
 }
 
 let cssConfig = {
-    test: /\.css$/i,
-    use: ['css-loader', { loader: 'postcss-loader', options: { postcssOptions: { plugins: postCSSPlugins } } }],
+  test: /\.css$/i,
+  use: [
+    'css-loader',
+    { loader: 'postcss-loader', options: { postcssOptions: { plugins: postCSSPlugins } } },
+  ],
 };
 
 let pages = fse
-    .readdirSync('./app')
-    .filter(function (file) {
-        return file.endsWith('.html');
-    })
-    .map(function (page) {
-        return new HtmlWebpackPlugin({
-            filename: page,
-            template: `./app/${page}`,
-        });
+  .readdirSync('./app')
+  .filter(function (file) {
+    return file.endsWith('.html');
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`,
     });
+  });
 
 //shared config object
 let config = {
-    entry: './app/assets/scripts/App.js',
-    plugins: pages,
-    module: {
-        rules: [
-            cssConfig,
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env'],
-                    },
-                },
-            },
-        ],
-    },
+  entry: './app/assets/scripts/App.js',
+  plugins: pages,
+  module: {
+    rules: [
+      cssConfig,
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
 
 if (currentTask == 'dev') {
-    cssConfig.use.unshift('style-loader');
-    (config.output = {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app'),
+  cssConfig.use.unshift('style-loader');
+  (config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app'),
+  }),
+    (config.devServer = {
+      watchFiles: ['./app/**/*.html'],
+      static: {
+        directory: path.join(__dirname, 'app'),
+      },
+      hot: true,
+      port: 3000,
+      host: '0.0.0.0',
     }),
-        (config.devServer = {
-            watchFiles: ['./app/**/*.html'],
-            static: {
-                directory: path.join(__dirname, 'app'),
-            },
-            hot: true,
-            port: 3000,
-            host: '0.0.0.0',
-        }),
-        (config.mode = 'development');
+    (config.mode = 'development');
 }
 
 if (currentTask == 'build') {
-    cssConfig.use.unshift(MiniCssExtractPlugin.loader);
-    (config.output = {
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'docs'),
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+  (config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'docs'),
+  }),
+    (config.mode = 'production'),
+    (config.optimization = {
+      splitChunks: { chunks: 'all' },
+      minimize: true,
+      minimizer: [`...`, new CssMinimizerPlugin()],
     }),
-        (config.mode = 'production'),
-        (config.optimization = {
-            splitChunks: { chunks: 'all' },
-            minimize: true,
-            minimizer: [`...`, new CssMinimizerPlugin()],
-        }),
-        config.plugins.push(new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' }), new RunAfterCompile());
+    config.plugins.push(
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' }),
+      new RunAfterCompile()
+    );
 }
 
 module.exports = config;
